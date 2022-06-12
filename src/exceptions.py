edfea9f6700e4cb1dc031add9a7ba97266e9a65f
wstanley <stanley_23@mail.ru>
2022-06-12 07:27:35 +0300

    Тест на проверку дискриминанта

--- src/exceptions.py
+++ src/exceptions.py
@@ -4,3 +4,11 @@ class ACoefficientEqualZeroError(Exception):
     def __init__(self) -> None:
         message = "Старший коэффициент не может быть равен нулю"
         super().__init__(message)
+
+
+class DiscriminantLowerThanZeroError(Exception):
+    """Исключение возникающее если дискрименант меньше нуля"""
+
+    def __init__(self) -> None:
+        message = "Дискрименант меньше нуля"
+        super().__init__(message)
--- tests/test_quadratic_equation.py
+++ tests/test_quadratic_equation.py
@@ -2,12 +2,27 @@
 
 import pytest
 
-from src.exceptions import ACoefficientEqualZeroError
+from src.exceptions import (
+    ACoefficientEqualZeroError,
+    DiscriminantLowerThanZeroError,
+)
 from src.quadratic_equation import QuadraticEquation
 
 
 class TestQuadraticEquation:
     """Тестируем решение квадратного уравнения"""
+
+    def test_discriminant_lower_than_zero(self) -> None:
+        """Проверяем что при дискриминанте меньше нуля решений нет"""
+        a = 1
+        b = 0
+        c = 1
+
+        with pytest.raises(DiscriminantLowerThanZeroError) as exc_info:
+            QuadraticEquation(a, b, c)
+
+        assert exc_info.typename == "DiscriminantLowerThanZeroError"
+        assert str(exc_info.value) == "Дискрименант меньше нуля"
 
     def test_coefficient_a_equal_zero(self) -> None:
         """Проверим, что для коэффициента a равным нулю решений нет"""
